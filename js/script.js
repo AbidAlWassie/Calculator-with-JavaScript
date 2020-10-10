2d30d2fc8f6f17530dc643b2208bfc5af1c43e56
Abid Al-Wasi <abidalwasi@outlook.com>
2020-10-10 18:53:34 +0600

    updated alongside everything

--- js/script.js
+++ js/script.js
@@ -105,3 +105,8 @@ allClearButton.addEventListener("click", button => {
   calculator.clear();
   calculator.updateDisplay();
 });
+
+deleteButton.addEventListener("click", button => {
+  calculator.delete();
+  calculator.updateDisplay();
+});
